442041d442695dcbc1b3381645843f147f52dac8
Vivek <ojhavivek24@gmail.com>
2025-05-27 17:15:15 +0530

    added assets

--- src/components/OrganizingCommittee.tsx
+++ src/components/OrganizingCommittee.tsx
@@ -1,4 +1,24 @@
-import { title } from "process";
+import chiefPatron from "../assets/director.jpg";
+import patron from "../assets/dean.jpeg";
+import charPerson from "../assets/chairPerson.jpg";
+import generalChairMember1 from "../assets/generalChairMember1.jpg";
+import generalChairMember2 from "../assets/generalChairMember2.jpg";
+import generalCoChairPerson1 from "../assets/generalCoChairPerson1.jpg";
+import generalCoChairPerson2 from "../assets/generalCoChairPerson2.jpg";
+import generalCoChairPerson3 from "../assets/generalCoChairPerson3.jpg";
+import programChairPerson1 from "../assets/programChairMember1.png";
+import programChairPerson2 from "../assets/programChairMember2.png";
+import programChairPerson3 from "../assets/programChairMember3.png";
+import programCoChairPerson1 from "../assets/programCoChairMember1.jpg";
+import programCoChairPerson2 from "../assets/programCoChairMember2.jpg";
+import programCoChairPerson3 from "../assets/programCoChairMember3.jpg";
+import publicityChairPerson1 from "../assets/publicityChairMember1.jpg";
+import publicityChairPerson2 from "../assets/publicityChairMember2.jpg";
+import publicityChairPerson3 from "../assets/publicityChairMember3.jpg";
+import publicityChairPerson4 from "../assets/publicityChairMember4.png";
+import organizingSecretaryPerson1 from "../assets/organizingSecretaryMember1.png";
+import organizingSecretaryPerson2 from "../assets/organizingSecretaryMember2.png";
+import organizingSecretaryPerson3 from "../assets/organizingSecretaryMember3.png";
 
 const OrganizingCommittee = () => {
   const mainHeads = [
@@ -6,16 +26,19 @@ const OrganizingCommittee = () => {
       title: "Chief Patron",
       fullName: "Prof. (Dr.) Karunesh Kumar Shukla",
       designation: "Director, MANIT Bhopal",
+      image:chiefPatron
     },
     {
       title: "Patron",
       fullName: "Prof. (Dr.) S. P. S. Rajput",
       designation: "Dean ID & IR",
+      image:patron
     },
     {
       title: "Chair Person",
       fullName: "Prof. (Dr.) Namita Srivastava",
       designation: "MANIT Bhopal, India",
+      image:charPerson
     },
   ];
 
@@ -23,10 +46,12 @@ const OrganizingCommittee = () => {
     {
       fullName: "Prof. (Dr.)  Sanjay Sharma",
       college: "MANIT Bhopal, India",
+      image:generalChairMember1
     },
     {
       fullName: "Prof. (Dr.) Debasis Kundu",
       college: "IIT Kanpur, India",
+      image:generalChairMember2
     },
   ];
 
@@ -34,14 +59,17 @@ const OrganizingCommittee = () => {
     {
       fullName: "Prof. (Dr.)  Sujoy Das",
       college: "MANIT Bhopal, India",
+      image:generalCoChairPerson1
     },
     {
       fullName: "Prof. (Dr.) Debjani Chakraborty",
       college: " IIT Kharagpur, India",
+      image:generalCoChairPerson2
     },
     {
       fullName: "Prof. (Dr.)  Madhvi Shakya",
       college: "MANIT Bhopal, India",
+      image:generalCoChairPerson3
     },
   ];
 
@@ -49,14 +77,17 @@ const OrganizingCommittee = () => {
     {
       fullName: "Dr. Usha Chouhan",
       college: "MANIT Bhopal, India",
+      image:programChairPerson1,
     },
     {
       fullName: "Dr. Chandan Kumar Verma",
       college: "MANIT Bhopal, India",
+      image:programChairPerson2,
     },
     {
       fullName: "Dr. Amit Bhagat",
       college: "MANIT Bhopal, India",
+      image:programChairPerson3,
     },
   ];
 
@@ -64,14 +95,17 @@ const OrganizingCommittee = () => {
     {
       fullName: "Dr. G. S. Thakur",
       college: "MANIT Bhopal, India",
+      image:programCoChairPerson1,
     },
     {
       fullName: "Dr. Manoj Jha",
       college: "MANIT Bhopal, India",
+      image:programCoChairPerson2,
     },
     {
       fullName: "Dr. Dheerendra Mishra",
       college: "MANIT Bhopal, India",
+      image:programCoChairPerson3,
     },
   ];
 
@@ -79,18 +113,22 @@ const OrganizingCommittee = () => {
     {
       fullName: "Dr. Pushpendra Kumar",
       college: "MANIT Bhopal, India",
+      image:publicityChairPerson1,
     },
     {
       fullName: "Dr. Jitendra Kumar",
       college: "MANIT Bhopal, India",
+      image:publicityChairPerson2,
     },
     {
       fullName: "Dr. S. Devidas",
       college: "MANIT Bhopal, India",
+      image:publicityChairPerson3,
     },
     {
       fullName: "Dr. Kuldeep Singh Yadav",
       college: "MANIT Bhopal, India",
+      image:publicityChairPerson4,
     },
   ];
 
@@ -98,14 +136,17 @@ const OrganizingCommittee = () => {
     {
       fullName: "Dr. Jay Kumar Jain",
       college: "MANIT Bhopal, India",
+      image:organizingSecretaryPerson1,
     },
     {
       fullName: "Dr. Suman Das",
       college: "MANIT Bhopal, India",
+      image:organizingSecretaryPerson2,
     },
     {
       fullName: "Dr. Subhankar Dutta",
       college: "MANIT Bhopal, India",
+      image:organizingSecretaryPerson3,
     },
   ];
 
@@ -128,7 +169,7 @@ const OrganizingCommittee = () => {
                 className="mt-12 pt-8 pb-12 border bg-slate-50 rounded-lg shadow hover:shadow-md "
               >
                 <div className="flex flex-col gap-8">
-                  <div className="h-36 w-36 rounded-full bg-slate-300 mx-auto"></div>
+                  <img src={data.image} alt={data.fullName} className="h-36 w-36 rounded-full mx-auto"/>
                   <div className="text-center">
                     <p>{data.title}</p>
                     <p>{data.fullName}</p>
@@ -151,7 +192,7 @@ const OrganizingCommittee = () => {
                   className="mb-8 px-4 py-2 bg-slate-50 shadow border-l-4 border-red-600 rounded-lg"
                 >
                   <div className="flex items-center gap-8">
-                    <div className="h-20 w-20 rounded-full bg-slate-400"></div>
+                    <img src={data.image} alt={data.fullName} className="h-20 w-20 rounded-full mx-auto"/>
                     <div>
                       <p>{data.fullName}</p>
                       <p>{data.college}</p>
@@ -174,7 +215,7 @@ const OrganizingCommittee = () => {
                   className="mb-8 px-4 py-2 bg-slate-50 shadow border-l-4 border-red-600 rounded-lg"
                 >
                   <div className="flex items-center gap-8">
-                    <div className="h-20 w-20 rounded-full bg-slate-400"></div>
+                     <img src={data.image} alt={data.fullName} className="h-20 w-20 rounded-full mx-auto"/>
                     <div>
                       <p>{data.fullName}</p>
                       <p>{data.college}</p>
@@ -195,7 +236,7 @@ const OrganizingCommittee = () => {
                   className="mb-8 px-4 py-2 bg-slate-50 shadow border-l-4 border-red-600 rounded-lg"
                 >
                   <div className="flex items-center gap-8">
-                    <div className="h-20 w-20 rounded-full bg-slate-400"></div>
+                    <img src={data.image} alt={data.fullName} className="h-20 w-20 rounded-full mx-auto"/>
                     <div>
                       <p>{data.fullName}</p>
                       <p>{data.college}</p>
@@ -220,7 +261,7 @@ const OrganizingCommittee = () => {
                   className="mb-8 px-4 py-2 bg-slate-50 shadow border-l-4 border-icosom-500 rounded-lg"
                 >
                   <div className="flex items-center gap-8">
-                    <div className="h-20 w-20 rounded-full bg-slate-400"></div>
+                     <img src={data.image} alt={data.fullName} className="h-20 w-20 rounded-full mx-auto"/>
                     <div>
                       <p>{data.fullName}</p>
                       <p>{data.college}</p>
@@ -241,7 +282,7 @@ const OrganizingCommittee = () => {
                   className="mb-8 px-4 py-2 bg-slate-50 shadow border-l-4 border-icosom-500 rounded-lg"
                 >
                   <div className="flex items-center gap-8">
-                    <div className="h-20 w-20 rounded-full bg-slate-400"></div>
+                    <img src={data.image} alt={data.fullName} className="h-20 w-20 rounded-full mx-auto"/>
                     <div>
                       <p>{data.fullName}</p>
                       <p>{data.college}</p>
@@ -264,7 +305,7 @@ const OrganizingCommittee = () => {
                   className="mb-8 px-4 py-2 bg-slate-50 shadow border-l-4 border-icosom-500 rounded-lg"
                 >
                   <div className="flex items-center gap-8">
-                    <div className="h-20 w-20 rounded-full bg-slate-400"></div>
+                    <img src={data.image} alt={data.fullName} className="h-20 w-20 rounded-full mx-auto"/>
                     <div>
                       <p>{data.fullName}</p>
                       <p>{data.college}</p>

a7a63ae88f94fabee500d81383fb656ef3de4ea0
Vivek <ojhavivek24@gmail.com>
2025-08-06 13:43:19 +0530

    added vercel file and ui fix in hero section

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -13,8 +13,8 @@ const Logo: React.FC<LogoProps> = ({ className = "" }) => {
     <div className={`flex items-center gap-2 ${className}`}>
       <img src={manitLogo} alt="Logo" className='w-16 h-16 md:w-24 md:h-24 rounded-full border border-black'/>
       <div>
-        <div className="text-2xl text-right font-bold tracking-tight text-icosom-500">ICOSOM</div>
-        <div className="text-xs text-right text-gray-500 leading-tight">
+        <div className="text-2xl text-center font-bold tracking-tight text-icosom-500">ICOSOM</div>
+        <div className="text-xs text-center text-gray-500 leading-tight">
           International Conference on<br />
           Statistics, Optimization & Machine Learning
         </div>

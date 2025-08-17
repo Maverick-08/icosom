8095801052c43108ba9110c2603e2b7501673b9e
Vivek <ojhavivek24@gmail.com>
2025-08-18 02:18:33 +0530

    updated footer section

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -83,10 +83,10 @@ const FooterSection = () => {
         </div>
         
         <div className="border-t border-gray-200 pt-8">
-          <p className="text-left text-gray-500 text-sm">The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
           <p className="text-center text-gray-500 text-sm">
             &copy; 2025 ICOSOM. All rights reserved. International Conference on Statistics, Optimization and Machine Learning.
           </p>
+          <p className='pt-2 text-center text-gray-500 text-sm cursor-pointer'>Built by <a className='underline' href="https://vivekojha.vercel.app/">Vivek Ojha</a></p>
         </div>
       </div>
     </footer>

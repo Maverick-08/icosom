f2c97338b53256053462f8bf61cc9257d0727815
Vivek <ojhavivek24@gmail.com>
2025-12-02 10:27:11 +0530

    added sponsor

--- src/components/OurSponsorSection.tsx
+++ src/components/OurSponsorSection.tsx
@@ -1,30 +1,7 @@
-import React from "react";
+import Sponsor1 from "../assets/sponsorLogo.png";
 
 const OurSponsorSection = () => {
-  // Sample sponsor data
-  const sponsors = [
-    { name: "TechCorp", logo: "https://placehold.co/600x400?text=TechCorp" },
-    {
-      name: "InnovateInc",
-      logo: "https://placehold.co/600x400?text=InnovateInc",
-    },
-    {
-      name: "DataSystems",
-      logo: "https://placehold.co/600x400?text=DataSystems",
-    },
-    { name: "CloudTech", logo: "https://placehold.co/600x400?text=CloudTech" },
-    {
-      name: "AI Solutions",
-      logo: "https://placehold.co/600x400?text=AI+Solutions",
-    },
-    { name: "DevTools", logo: "https://placehold.co/600x400?text=DevTools" },
-    { name: "CyberSafe", logo: "https://placehold.co/600x400?text=CyberSafe" },
-    { name: "WebFlow", logo: "https://placehold.co/600x400?text=WebFlow" },
-    { name: "DataMine", logo: "https://placehold.co/600x400?text=DataMine" },
-    { name: "TechHub", logo: "https://placehold.co/600x400?text=TechHub" },
-  ];
-
-  const duplicatedSponsors = [...sponsors, ...sponsors]; // For infinite scrolling
+  
 
   return (
     <>
@@ -32,23 +9,10 @@ const OurSponsorSection = () => {
         <h1 className="text-4xl font-medium text-center mb-4">Our Sponsors</h1>
         <div className="w-24 h-1 bg-gradient-to-r from-icosom-500 to-icosom-teal mx-auto mb-6"></div>
 
-        <p className="text-center text-gray-500">To be announced soon</p>
+       <div className="pt-12 lg:pt-24 flex justify-center">
+        <img src={Sponsor1} alt="ANSR Logo"  className="w-70 lg:w-80 h-50 lg:h-60"/>
+       </div>
 
-        {/* Infinite Logo Marquee */}
-        {/* <div className="relative overflow-hidden">
-          <div className="flex pt-20 pb-9 animate-marquee space-x-8">
-            {duplicatedSponsors.map((sponsor, index) => (
-              <div key={index} className="flex-shrink-0 p-5">
-                <img
-                  src={sponsor.logo}
-                  alt={`${sponsor.name} logo`}
-                  className="h-[18vh] w-auto object-contain mx-auto rounded-lg"
-                  loading="smooth"
-                />
-              </div>
-            ))}
-          </div>
-        </div> */}
       </div>
     </>
   );

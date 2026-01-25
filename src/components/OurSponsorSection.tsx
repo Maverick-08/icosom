8c53891878bfc4875b9242f386e64ecd36e5640c
Vivek <ojhavivek24@gmail.com>
2026-01-25 19:28:55 +0530

    added sponsor

--- src/components/OurSponsorSection.tsx
+++ src/components/OurSponsorSection.tsx
@@ -1,4 +1,5 @@
 import Sponsor1 from "../assets/sponsorLogo.png";
+import Sponsor2 from "../assets/sponsorLogo2.png.jpeg";
 
 const OurSponsorSection = () => {
   
@@ -9,8 +10,9 @@ const OurSponsorSection = () => {
         <h1 className="text-4xl font-medium text-center mb-4">Our Sponsors</h1>
         <div className="w-24 h-1 bg-gradient-to-r from-icosom-500 to-icosom-teal mx-auto mb-6"></div>
 
-       <div className="pt-12 lg:pt-24 flex justify-center">
-        <img src={Sponsor1} alt="ANSR Logo"  className="w-70 lg:w-80 h-50 lg:h-60"/>
+       <div className="pt-12 lg:pt-24 flex justify-center items-center gap-8">
+        <img src={Sponsor1} alt="ANSR Logo"  className="w-70 lg:w-80 h-50 lg:h-40"/>
+        <img src={Sponsor2} alt="ANSR Logo"  className="w-70 lg:w-80 h-50 lg:h-60"/>
        </div>
 
       </div>
